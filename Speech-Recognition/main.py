828c4fb9b623182193957fe1f9bb75a489baadb4
AdamPostMontjoie <57641108+AdamPostMontjoie@users.noreply.github.com>
2025-11-20 15:27:34 -0500

    vosk

--- Speech-Recognition/main.py
+++ Speech-Recognition/main.py
@@ -1,104 +1,55 @@
-#!/usr/bin/env python3
+import vosk
+import pyaudio
+import json
+import serial
 
-# NOTE: this example requires PyAudio because it uses the Microphone class
 
-import os
+# Here I have downloaded this model to my PC, extracted the files 
+# and saved it in local directory
+# Set the model path
+model_path = "vosk-model-small-en-us-0.15"
+# Initialize the model with model-path
+model = vosk.Model(model_path)
 
-import speech_recognition as sr
+accepted_words = '["up", "down","left" ,"right", "terminate", "[unk]"]'
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+#if you don't want to download the model, just mention "lang" argument 
+#in vosk.Model() and it will download the right  model, here the language is 
+#US-English
+#model = vosk.Model(lang="en-us")
+# Create a recognizer
+esp = serial.Serial('/dev/tty.usbserial-1440',115200)
 
-# recognize speech using Sphinx
-try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+rec = vosk.KaldiRecognizer(model, 16000,accepted_words)
+# Open the microphone stream
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16,
+                channels=1,
+                rate=16000,
+                input=True,
+                frames_per_buffer=8192)
+# Specify the path for the output text file
+output_file_path = "recognized_text.txt"
+# Oen a text file in write mode using a 'with' block
+print("Listening for speech. Say 'Terminate' to stop.")
+    # Start streaming and recognize speech
+while True:
+        data = stream.read(4096)#read in chunks of 4096 bytes
+        if rec.AcceptWaveform(data):#accept waveform of input voice
+            # Parse the JSON result and get the recognized text
+            result = json.loads(rec.Result())
+            recognized_text = result['text']
+            
+            # Write recognized text to the file
+            print(recognized_text)
+            esp.write(recognized_text.encode('utf-8'))
+            # Check for the termination keyword
+            if "terminate" in recognized_text.lower():
+                print("Termination keyword detected. Stopping...")
+                break
+# Stop and close the stream
+stream.stop_stream()
+stream.close()
 
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-# recognize speech using Google Cloud Speech
-# Before run, create local authentication credentials (``gcloud auth application-default login``)
-try:
-    print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio))
-except sr.UnknownValueError:
-    print("Google Cloud Speech could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Cloud Speech service; {0}".format(e))
-
-# recognize speech using Wit.ai
-WIT_AI_KEY = "INSERT WIT.AI API KEY HERE"  # Wit.ai keys are 32-character uppercase alphanumeric strings
-try:
-    print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
-except sr.UnknownValueError:
-    print("Wit.ai could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Wit.ai service; {0}".format(e))
-
-# recognize speech using Microsoft Bing Voice Recognition
-BING_KEY = "INSERT BING API KEY HERE"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
-
-# recognize speech using Microsoft Azure Speech
-AZURE_SPEECH_KEY = "INSERT AZURE SPEECH API KEY HERE"  # Microsoft Speech API keys 32-character lowercase hexadecimal strings
-try:
-    print("Microsoft Azure Speech thinks you said " + r.recognize_azure(audio, key=AZURE_SPEECH_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Azure Speech could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Azure Speech service; {0}".format(e))
-
-# recognize speech using Houndify
-HOUNDIFY_CLIENT_ID = "INSERT HOUNDIFY CLIENT ID HERE"  # Houndify client IDs are Base64-encoded strings
-HOUNDIFY_CLIENT_KEY = "INSERT HOUNDIFY CLIENT KEY HERE"  # Houndify client keys are Base64-encoded strings
-try:
-    print("Houndify thinks you said " + r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY))
-except sr.UnknownValueError:
-    print("Houndify could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Houndify service; {0}".format(e))
-
-# recognize speech using IBM Speech to Text
-IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
-try:
-    print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
-except sr.UnknownValueError:
-    print("IBM Speech to Text could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from IBM Speech to Text service; {0}".format(e))
-
-# recognize speech using whisper
-try:
-    print("Whisper thinks you said " + r.recognize_whisper(audio, language="english"))
-except sr.UnknownValueError:
-    print("Whisper could not understand audio")
-except sr.RequestError as e:
-    print(f"Could not request results from Whisper; {e}")
-
-# recognize speech using Whisper API
-OPENAI_API_KEY = "INSERT OPENAI API KEY HERE"
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-try:
-    print(f"OpenAI Whisper API thinks you said {r.recognize_openai(audio)}")
-except sr.RequestError as e:
-    print(f"Could not request results from OpenAI Whisper API; {e}")
+# Terminate the PyAudio object
+p.terminate()
